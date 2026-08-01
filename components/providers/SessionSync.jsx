478ede387d074253e038bae7731ca084e48d1097
Noyon Sarker <developernoyon9@gmail.com>
2026-08-01 19:35:01 +0600

    udpate

--- components/providers/SessionSync.jsx
+++ components/providers/SessionSync.jsx
@@ -4,26 +4,25 @@ import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setUser, setToken } from "@/store/user";
+import { useProfileQuery } from "@/store/auth";
 
 export default function SessionSync() {
   const { data: session } = useSession();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (session?.user) {
-      dispatch(
-        setUser({
-          id: session.user.id,
-          name: session.user.name,
-          email: session.user.email,
-          role: session.user.role,
-          profileImageUrl: session.user.profileImageUrl,
-          permissions: session.user.permissions,
-        })
-      );
+    if (session?.user?.backendToken) {
       dispatch(setToken(session.user.backendToken));
     }
   }, [session, dispatch]);
+
+  const { data: profileData } = useProfileQuery();
+
+  useEffect(() => {
+    if (profileData?.data) {
+      dispatch(setUser(profileData.data));
+    }
+  }, [profileData, dispatch]);
 
   return null;
 }
