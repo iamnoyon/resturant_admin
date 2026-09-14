a8a6993f35abb623ca2a1dfabcbe5eec71a68caa
Noyon Sarker <developernoyon9@gmail.com>
2026-09-14 22:48:43 +0600

    fixed

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,4 +1,4 @@
 export const siteConfig = {
-    baseUrl: "http://localhost:8001/api"
-    // baseUrl: "https://resturant-backend-3khk.onrender.com/api"
+    // baseUrl: "http://localhost:8001/api"
+    baseUrl: "https://resturant-backend-3khk.onrender.com/api"
 }
